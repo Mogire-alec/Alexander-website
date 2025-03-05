4541f5d8d557b3c06f1eb954d79570eb1b768e86
Alex <alexandermogire009@gmail.com>
2025-03-05 11:36:35 +0300

    adds

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-btn').addEventListener('mouseover', function() {
+    this.innerHTML = "Click to Connect!";
+});
+
+document.getElementById('contact-btn').addEventListener('mouseout', function() {
+    this.innerHTML = "Contact Me";
+});
